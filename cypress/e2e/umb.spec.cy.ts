926a898d6bef1ab657966b673b32ae0c2f094281
lumin <lumin7135@proton.me>
2024-05-27 14:50:29 +0800

    fix test error

--- cypress/e2e/umb.spec.cy.ts
+++ cypress/e2e/umb.spec.cy.ts
@@ -2,7 +2,7 @@ describe('Umbrella home test', function () {
   it('Home page can be opened', function () {
     cy.visit('http://localhost:5000');
     cy.contains('Welcome');
-    cy.contains('Research is life itself');
+    cy.contains('Umbrella');
   });
 
   it('sign up page can be navigated to', function () {
